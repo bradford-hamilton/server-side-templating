7642b03b7fe25e5e299603110d4ac3fbafcc4fba
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-06-03 16:05:27 -0600

    add working express/handlebars app

---
+++ data.js
@@ -0,0 +1,22 @@
+// How to hate Bennett
+var howTo = {
+  title: 'How to hate bennett',
+  firstSteps: ['Look at his face',
+          'Talk to him',
+          'Spend any amount of time with him',
+        ],
+  secondSteps: [
+          'Ask him a question',
+          'Hear his voice',
+          'Try pronouncing his last name',
+        ],
+  thirdSteps: [
+          'Think about him',
+          'Just knowing he exists',
+          'He is probably voting for Trump',
+          'Also hate Daniel because of all the same reasons'
+        ],
+  images: '/images/bennett.png'
+};
+
+module.exports = howTo;
---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var reasons = require('../data');
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: reasons.title });
+});
+
+/* GET how-to-hate-Bennett route */
+router.get('/how-to-hate-bennett', function (request, response) {
+  response.render('how-to-hate-bennett', { steps: reasons.firstSteps,
+                                          title: "First Steps",
+                                          back: '/',
+                                          next: '/how-to-hate-bennett/keep-going'
+                                         });
+});
+
+router.get('/how-to-hate-bennett/keep-going', function(request, response) {
+  response.render('how-to-hate-bennett', { steps: reasons.secondSteps,
+                                           title: "Second Steps",
+                                           back: '/how-to-hate-bennett',
+                                           next: '/how-to-hate-bennett/keep-going/last-steps'
+                                         });
+});
+
+router.get('/how-to-hate-bennett/keep-going/last-steps', function(request, response) {
+  response.render('how-to-hate-bennett', { steps: reasons.thirdSteps,
+                                           title: "Last Steps!",
+                                           back: '/how-to-hate-bennett/keep-going',
+                                           next: '/how-to-hate-bennett/keep-going',
+                                           image: reasons.images
+                                         });
+});
+
+
+
+module.exports = router;
